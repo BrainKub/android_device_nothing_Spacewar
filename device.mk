21b271c852dd5d60e989ea010b18c792544e2abe
Bruno Martins <bgcngm@users.noreply.github.com>
2024-08-13 13:45:28 -0400

    Spacewar: Move to new RFS install_symlink targets
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: BrainKub <fyhtjnt@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 
